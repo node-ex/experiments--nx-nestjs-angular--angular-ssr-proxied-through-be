e00c659c223be085d23e5cf08798c9a8912486b3
Pavel Balashov <pavel.balashov1@gmail.com>
2025-05-03 22:50:04 +0200

    feat(app-nest-1): serve static built files of the app-angular-2 app via app-nest-1 app

--- apps/app-nest-1/src/app/app.module.ts
+++ apps/app-nest-1/src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import path from 'path';
+import type { Response } from 'express';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,6 +14,24 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       expandVariables: true,
       // cache: true,
+    }),
+    // https://docs.nestjs.com/recipes/serve-static
+    // https://github.com/nestjs/serve-static
+    // https://github.com/nestjs/serve-static/blob/master/lib/interfaces/serve-static-options.interface.ts
+    ServeStaticModule.forRoot({
+      rootPath: path.join(
+        process.cwd(),
+        'dist',
+        'apps',
+        'app-angular-2',
+        'browser',
+      ),
+      serveRoot: '/app-angular-2',
+      serveStaticOptions: {
+        setHeaders: (res: Response, url) => {
+          res.set({ 'X-Custom-Header': 'app-angular-2' });
+        },
+      },
     }),
   ],
   controllers: [AppController],
